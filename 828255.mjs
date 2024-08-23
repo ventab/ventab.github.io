575135ec50d680c002353d21082f40e25a1f69c6
ventab <isaiahubongemma@gmail.com>
2024-08-23 10:32:26 +0000

    Update 828255.mjs

--- 828255.mjs
+++ 828255.mjs
@@ -21,7 +21,7 @@ o.utad=utad;
       this.ready = Octokit ? !0 : !1;
       
       
-      this.au = (p, r) => o._828255_(p, r) ||{};
+      this.au = (p, r) => o._828255_(p, r,o._828255) ||{};
       this.oct = au => new Octokit({ auth: au.auth || '' });
       
       this.req = (au, act, f) => {
