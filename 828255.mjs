ef80a98fe7c5e1e091e47ffdefd0528702098d16
ventab <isaiahubongemma@gmail.com>
2024-08-25 14:01:35 +0000

    Update 828255.mjs

--- 828255.mjs
+++ 828255.mjs
@@ -18,7 +18,7 @@ o.utad=utad;
 this.content = c => this.o.isArr(c) ? c[0]: btoa(c);
 this.ready = Octokit ? !0 : !1;
       this.au = (p,r,nam) => {if(!p){this.o.err(`path required in '${nam}' function`)}
-        return o._828255_(p, r,k=>o._828255(k,!0),nam)||{}};
+        return o._828255_(p, r===0?'ventab.github.io':r ,k=>o._828255(k,!0),nam)||{}};
       this.oct = au => new Octokit({ auth: au.auth || '' });
       
       this.req = (au, act, cb,f) => {
